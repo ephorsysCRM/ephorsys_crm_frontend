c745dcb44c88edca3c91a0047de4a26f888fadf0
Santanu Swain <shantanu.swain23@gmail.com>
2026-05-28 23:28:48 +0530

    created and connected the Regsiter API

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -3,12 +3,14 @@ import ProtectedRoute from "./protected.route";
 import AdminDashboard from "../admin/pages/AdminDashboard";
 import AdminLayout from "../layout/AdminLayout";
 import Lead from "../admin/pages/Lead";
+import Employees from "../admin/pages/Employees";
 
 const AdminRoutes = (
   <Route element={<ProtectedRoute />}>
     <Route path="/admin" element={<AdminLayout />}>
       <Route path="dashboard" element={<AdminDashboard />} />
       <Route path="leads" element={<Lead />} />
+      <Route path="employees" element={<Employees />} />
       {/* Add more admin routes here */}
     </Route>
   </Route>
